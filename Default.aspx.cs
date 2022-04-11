79776575b6a286d8b6215a0b610ba221eed5a37b
rboswellj <rboswellj@gmail.com>
2022-04-10 21:22:05 -0400

    Can now perform the same operation multiple times in a chain without hitting equal, but if you change the operation it seems to break it.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,6 +14,81 @@ namespace AspNetCalc2
         public static string num2 = String.Empty;
         public static char operation;
         public static double result;
+        public static bool needClear = false;
+
+        public void EnterNumber(string num)
+        {
+            if (needClear)
+            {
+                calcDisplay.Text = String.Empty;
+                input = String.Empty;
+                needClear = false;
+            }
+            string value = num;
+            input += value;
+            calcDisplay.Text += value;
+            errorAlert.Text = String.Empty;
+        }
+
+        public void GetEquals(string number1, string number2, char op)
+        {
+            // Switch statement on operation dicates which operation to perform
+            switch (op)
+            {
+                case '+':
+                    result = double.Parse(number1) + double.Parse(number2);
+                    break;
+                case '-':
+                    result = double.Parse(number1) - double.Parse(number2);
+                    break;
+                case '*':
+                    result = double.Parse(number1) * double.Parse(number2);
+                    break;
+                case '/':
+                    if(number2 != "0")
+                    {
+                        result = double.Parse(number1) / double.Parse(number2);
+                        break;
+                    }
+                    else
+                    {
+                        result = double.Parse(number1);
+                        errorAlert.Text = "Cannot divide by zero.";
+                        errorAlert.Visible = true;
+                        break;
+                    }
+                default:
+                    result = double.Parse(num1);
+                    break;
+            }
+            // Assigns result to calculator display. Also assigns it to input, so operation can continue on result.
+            calcDisplay.Text = Convert.ToString(result);
+            input = String.Empty;
+            num1 = Convert.ToString(result);
+            num2 = String.Empty;
+        }
+
+        public void SetOperation(char op)
+        {
+            if(calcDisplay.Text != String.Empty)
+            {
+                if (num1 == String.Empty)
+                {
+                    num1 = input;
+                    input = String.Empty;
+                    calcDisplay.Text = String.Empty;
+                    operation = op;
+                }
+                else
+                {
+                    num2 = input;
+                    GetEquals(num1, num2, op);
+                    needClear = true;
+                }
+            
+                errorAlert.Text = String.Empty;
+            }
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -21,92 +96,63 @@ namespace AspNetCalc2
             calcDisplay.Attributes.Add("readonly", "readonly");
         }
 
+        #region Number Buttons
+
         protected void OneBtn_Click(object sender, EventArgs e)
         {
-            string value = "1";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber("1");
         }
 
         protected void TwoBtn_Click(object sender, EventArgs e)
         {
-            string value = "2";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber("2");
         }
 
         protected void ThreeBtn_Click(object sender, EventArgs e)
         {
-            string value = "3";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber("3");
         }
 
         protected void FourBtn_Click(object sender, EventArgs e)
         {
-            string value = "4";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber("4");
         }
 
         protected void FiveBtn_Click(object sender, EventArgs e)
         {
-            string value = "5";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber("5");
         }
 
         protected void SixBtn_Click(object sender, EventArgs e)
         {
-            string value = "6";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber("6");
         }
 
         protected void SevenBtn_Click(object sender, EventArgs e)
         {
-            string value = "7";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber("7");
         }
 
         protected void EightBtn_Click(object sender, EventArgs e)
         {
-            string value = "8";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber("8");
         }
 
         protected void NineBtn_Click(object sender, EventArgs e)
         {
-            string value = "9";
-            input += value;
-            calcDisplay.Text += value; 
-            errorAlert.Text = String.Empty;
+            EnterNumber("9");
         }
 
         protected void ZeroBtn_Click(object sender, EventArgs e)
         {
-            string value = "0";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber("0");
         }
+
         protected void DotBtn_Click(object sender, EventArgs e)
         {
-            string value = ".";
-            input += value;
-            calcDisplay.Text += value;
-            errorAlert.Text = String.Empty;
+            EnterNumber(".");
         }
+        #endregion
 
         protected void ClearBtn_Click(object sender, EventArgs e)
         {
@@ -118,81 +164,39 @@ namespace AspNetCalc2
             num2 = value;
         }
 
+        #region operator buttons
         protected void PlusBtn_Click(object sender, EventArgs e)
         {
-            num1 = input;
-            operation = '+';
-            input = String.Empty;
-            calcDisplay.Text = String.Empty;
-            errorAlert.Text = String.Empty;
+            SetOperation('+');
         }
         protected void MinusBtn_Click(object sender, EventArgs e)
         {
-            num1 = input;
-            operation = '-';
-            input = String.Empty;
-            calcDisplay.Text = String.Empty;
-            errorAlert.Text = String.Empty;
+            SetOperation('-');
         }
 
         protected void TimesBtn_Click(object sender, EventArgs e)
         {
-            num1 = input;
-            operation = '*';
-            input = String.Empty;
-            calcDisplay.Text = String.Empty;
-            errorAlert.Text = String.Empty;
+            SetOperation('*');
         }
 
         protected void DivideBtn_Click(object sender, EventArgs e)
         {
-            num1 = input;
-            operation = '/';
-            input = String.Empty;
-            calcDisplay.Text = String.Empty;
-            errorAlert.Text = String.Empty;
+            SetOperation('/');
         }
 
         protected void EqBtn_Click(object sender, EventArgs e)
         {
-            num2 = input;
+            if(num2 == String.Empty)
+            {
+                num2 = input;
+            }
             // Insure that equals operation will only proceed if 2 values assigned
             if(num1 != String.Empty && num2 != String.Empty)
             {
-                // Switch statement on operation dicates which operation to perform
-                switch (operation)
-                {
-                    case '+':
-                        result = double.Parse(num1) + double.Parse(num2);
-                        break;
-                    case '-':
-                        result = double.Parse(num1) - double.Parse(num2);
-                        break;
-                    case '*':
-                        result = double.Parse(num1) * double.Parse(num2);
-                        break;
-                    case '/':
-                        try
-                        {
-                            result = double.Parse(num1) / double.Parse(num2);
-                            break;
-                        }
-                        catch
-                        {
-                            result = double.Parse(num1);
-                            errorAlert.Text = "Cannot divide by zero.";
-                            break;
-                        }
-                    default:
-                        result = double.Parse(num1);
-                        break;
-                }
-                // Assigns result to calculator display. Also assigns it to input, so operation can continue on result.
-                calcDisplay.Text = Convert.ToString(result);
-                input = Convert.ToString(result);
-
+                GetEquals(num1, num2, operation);
             }
         }
 
+        #endregion
     }
 }
